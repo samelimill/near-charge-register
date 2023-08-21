c9732e947837740f53337e28b5cb50747d2c1db2
samelimill <samelimill@gmail.com>
2023-08-21 12:53:56 -0700

    Added function to compare each row's arrow to the current time and change color accordingly

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,33 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(function(){
+
+  //Display current date on top of page
+  var today = dayjs();
+  $('#currentDay').text(today.format('dddd, MMM D, YYYY'));
+
+  //Fetch current hour for comparison to sheet
+  var currentHour = parseInt(today.format('hh'));
+  console.log(currentHour);
+
+  $('.row').each(function (i){
+    var rowHour = parseInt($(this).attr('data-hour'));
+    console.log(rowHour);
+    if (rowHour === currentHour){
+      $(this).addClass('present');
+    } else if (rowHour > currentHour) {
+      $(this).addClass('future');
+    } else if (rowHour < currentHour) {
+      $(this).addClass('past');
+    };
+  });  
+    
+  //Compare fetched hour to rows, color coded for past/present/future
+ 
+
+});
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
